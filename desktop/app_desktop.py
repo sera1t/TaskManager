5bceb94b237f722ec60db2077aaab0f53b28d12a
sera1t <58zm@mail.ru>
2024-11-02 20:46:22 +0300

    [Added] version 0.2 add display auth, no functional display

--- desktop/app_desktop.py
+++ desktop/app_desktop.py
@@ -1,7 +1,14 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt6.QtCore import Qt
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
+    QPushButton, QSizePolicy, QStatusBar, QWidget)
 
 from Entity import Task, User
 
@@ -10,20 +17,79 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Task Manager')
+        self.setFixedSize(QSize(500, 302))
 
-        user = self.get_user()
+        self.centralwidget = QWidget(self)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.input_login = QLineEdit(self.centralwidget)
+        self.input_login.setObjectName(u"input_login")
+        self.input_login.setGeometry(QRect(171, 85, 133, 22))
+        self.input_password = QLineEdit(self.centralwidget)
+        self.input_password.setObjectName(u"input_password")
+        self.input_password.setGeometry(QRect(171, 135, 133, 22))
+        self.Auth_Text = QLabel(self.centralwidget)
+        self.Auth_Text.setObjectName(u"Auth_Text")
+        self.Auth_Text.setGeometry(QRect(171, 41, 72, 16))
+        self.Auth_Text.setStyleSheet(u".QLabel{\n"
+                                     "	color: red;\n"
+                                     "	font-size: 12px;\n"
+                                     "}")
+        self.Get_Login = QLabel(self.centralwidget)
+        self.Get_Login.setObjectName(u"Get_Login")
+        self.Get_Login.setGeometry(QRect(171, 63, 82, 16))
+        self.Get_Password = QLabel(self.centralwidget)
+        self.Get_Password.setObjectName(u"Get_Password")
+        self.Get_Password.setGeometry(QRect(171, 113, 90, 16))
+        self.btn_auth = QPushButton(self.centralwidget)
+        self.btn_auth.setObjectName(u"btn_auth")
+        self.btn_auth.setGeometry(QRect(171, 163, 108, 36))
+        self.btn_auth.setStyleSheet(u"QPushButton {\n"
+            "	background-color: rgb(255, 253, 253);\n"
+            "	border-radius: 5px;\n"
+            "	padding: 10px 10px;\n"
+            "	color: rgb(55, 107, 113);\n"
+            "}\n"
+            "\n"
+            "QPushButton:hover {\n"
+            "	color: rgb(45, 97, 100);\n"
+            "	border: 1px  solid rgb(55, 107, 113);\n"
+            "	cursor: pointer;\n"
+            "}")
+        self.btn_auth.clicked.connect(self.auth)
+        self.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(self)
+        self.statusbar.setObjectName(u"statusbar")
+        self.setStatusBar(self.statusbar)
 
-        widget = QLabel(user.username)
-        font = widget.font()
-        font.setPointSize(30)
-        widget.setFont(font)
-        widget.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        self.retranslateUi(self)
 
-        self.setCentralWidget(widget)
+        QMetaObject.connectSlotsByName(self)
 
-    def get_user(self):
-        user = User.UserEntity({'username': 'log', 'password': 'log'})
-        return user.check_user_db()['user']
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Task Manager", None))
+        self.Auth_Text.setText(QCoreApplication.translate("MainWindow",
+                                                          u"Авторизация",
+                                                          None))
+        self.Get_Login.setText(QCoreApplication.translate("MainWindow",
+                                                          u"Введите логин:",
+                                                          None))
+        self.Get_Password.setText(QCoreApplication.translate("MainWindow",
+                                                             u"Введите пароль:",
+                                                             None))
+        self.btn_auth.setText(QCoreApplication.translate("MainWindow",
+                                                         u"Авторизоваться",
+                                                         None))
+
+    def auth(self):
+        user = User.UserEntity({
+            'username': self.input_login.text(),
+            'password': self.input_password.text()
+        })
+        check_user = user.check_user_db()
+        if check_user['status']:
+            self.statusbar.showMessage('User found')
+        else:
+            self.statusbar.showMessage('User not found')
 
 app = QApplication(sys.argv)
 
